d509c20e0fa406e05ee8c08b78b6e91300f473f9
kinkax <jawahar.reddy007@gmail.com>
2016-04-03 23:35:30 +0530

    fixed indentation, found one error
    extra '{' at the end

--- WEB-INF/classes/LoginServlet.java
+++ WEB-INF/classes/LoginServlet.java
@@ -18,7 +18,6 @@ import java.sql.*;
 */
 
 public class LoginServlet extends HttpServlet {  
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)  
                            throws ServletException, IOException {  
         response.setContentType("text/html");  
@@ -27,108 +26,77 @@ public class LoginServlet extends HttpServlet {
         //request.getRequestDispatcher("index.html").include(request, response);  
            
         // Incase there is already a LOGIN-FAILED Cookie. It is deleted
-            Cookie ck = new Cookie("loginfailed", "");
-            ck.setMaxAge(0); 
-            response.addCookie(ck);
+        Cookie ck = new Cookie("loginfailed", "");
+        ck.setMaxAge(0); 
+        response.addCookie(ck);
 
-
-
-    String name=request.getParameter("username");  
-    String password=request.getParameter("password");  
-    String errorMsg = null;
-    if(name == null || name.equals("")){
-        errorMsg ="UsernameNull";
+        String name=request.getParameter("username");  
+        String password=request.getParameter("password");  
+        String errorMsg = null;
+        if(name == null || name.equals("")) {
+            errorMsg ="UsernameNull";
         }
-    if(password == null || password.equals("")){
-         errorMsg = "PasswordNull";
-     }
-    if(errorMsg != null){
-         
-               Cookie cookiee = new Cookie("loginfailed", errorMsg);
-                cookiee.setMaxAge(25*60); 
-                response.addCookie(cookiee);
-                response.sendRedirect("/index.html");
-           
-    }
-else
-{
+        if(password == null || password.equals("")) {
+            errorMsg = "PasswordNull";
+        }
+        if(errorMsg != null) {
+            Cookie cookiee = new Cookie("loginfailed", errorMsg);
+            cookiee.setMaxAge(25*60); 
+            response.addCookie(cookiee);
+            response.sendRedirect("/index.html");
+        }
+        else {
           // JDBC driver name and database URL
-       String JDBC_DRIVER="oracle.jdbc.driver.OracleDriver";  
-       String DB_URL="jdbc:oracle:thin:@localhost:1521:XE";  //--___REQUIRES TESTING!!!____---
-       //  Database credentials
-      String USER = "sys";
-      String PASS = "2710";   //JAWAHAR: You will have to change this for your own database during testing
-
-      //Change this BELOW to change the HTML file Format
-      PreparedStatement ps = null;
-         ResultSet rs = null;
- try{
-         // Register JDBC driver
-         Class.forName(JDBC_DRIVER);
-         //Open the Connection
-         Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-         //Preparing Query
-
-            ps = conn.prepareStatement("select * from USER_MAIN where USER_ID=? and PASSWORD=?");
-            ps.setString(1, name);
-            ps.setString(2, password);
-            rs = ps.executeQuery();
-             
-            if(rs != null && rs.next())   // Check if rs.next() Usage is right/.....
-            {
-                  // out.print("You are successfully logged in!");  
-                   // out.print("<br>Welcome, "+name);  
-                Cookie cookey=new Cookie("username",name);  
-                 response.addCookie(cookey); 
-                 cookey.setMaxAge(25 * 60);             // 25 minutes.
-                response.sendRedirect("/index.html");
+            String JDBC_DRIVER="oracle.jdbc.driver.OracleDriver";  
+            String DB_URL="jdbc:oracle:thin:@localhost:1521:XE";  //--___REQUIRES TESTING!!!____---
+               //  Database credentials
+            String USER = "sys";
+            String PASS = "2710";   //JAWAHAR: You will have to change this for your own database during testing
+              //Change this BELOW to change the HTML file Format
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try{
+                // Register JDBC driver
+                Class.forName(JDBC_DRIVER);
+                //Open the Connection
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                //Preparing Query
+                ps = conn.prepareStatement("select * from USER_MAIN where USER_ID=? and PASSWORD=?");
+                ps.setString(1, name);
+                ps.setString(2, password);
+                rs = ps.executeQuery();
+                 
+                if(rs != null && rs.next()) {    // Check if rs.next() Usage is right/.....
+                    // out.print("You are successfully logged in!");  
+                    // out.print("<br>Welcome, "+name);  
+                    Cookie cookey=new Cookie("username",name);  
+                    response.addCookie(cookey); 
+                    cookey.setMaxAge(25 * 60);             // 25 minutes.
+                    response.sendRedirect("/index.html");
+                }
+                else {
+                    errorMsg="UserOrPasswordNotValid";
+                    Cookie cooki = new Cookie("loginfailed", errorMsg);
+                    cooki.setMaxAge(25*60); 
+                    response.addCookie(cooki);
+                    response.sendRedirect("/index.html");
+                }
+           }
+            catch(Exception e) {
+                e.printStackTrace();
             }
-            else
-            {
-                 errorMsg="UserOrPasswordNotValid";
-                 Cookie cooki = new Cookie("loginfailed", errorMsg);
-                cooki.setMaxAge(25*60); 
-                response.addCookie(cooki);
-                response.sendRedirect("/index.html");
-            }
-           
 
-    }catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-
-    finally
-    {
-        try 
-        {
-            rs.close();
-            ps.close();
-            out.close(); 
-        } 
-        catch (SQLException e) 
-        {
-            e.printStackTrace();
-        }
-    }     
-
-          /*if(password.equals("admin123")){  
-            out.print("You are successfully logged in!");  
-             out.print("<br>Welcome, "+name);  
-              
-            Cookie ck=new Cookie("username",name);  
-            response.addCookie(ck);  
-        }
-        else{  
-            out.print("Username and password do not match");  
-           // request.getRequestDispatcher("index.html").include(request, response);  
-        }*/  
-          
-         
-}  
+            finally {
+                if(rs!=null) rs.close();
+                if(ps!=null) ps.close();
+                if(out!=null) out.close(); 
+            } 
+            
+        }     
+    }  
 
    //<!-- Write doGet function to allow  such statements:  <a href="loginPage">TEST</a> that request Get function to work-->
+   // ########  ^^^^^  we dont need it ########
   
 }
 
-}
