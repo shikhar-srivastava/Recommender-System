c73880fc162e09dbf34e8955c3809c14f091ad35
kinkax <jawahar.reddy007@gmail.com>
2016-04-03 14:12:29 +0530

    servlet basic

---
+++ WEB-INF/classes/LoginServlet.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.Cookie;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+
+public class LoginServlet extends HttpServlet {  
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+                           throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+          
+        request.getRequestDispatcher("index.html").include(request, response);  
+          
+        String name=request.getParameter("username");  
+        String password=request.getParameter("password");  
+          
+        if(password.equals("admin123")){  
+            out.print("You are successfully logged in!");  
+            out.print("<br>Welcome, "+name);  
+              
+            Cookie ck=new Cookie("username",name);  
+            response.addCookie(ck);  
+        }else{  
+            out.print("sorry, username or password error!");  
+            request.getRequestDispatcher("index.html").include(request, response);  
+        }  
+          
+        out.close();  
+    }  
+  
+}
