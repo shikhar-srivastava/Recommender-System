1766eddfeeeb374705daf6fbd2298b280b6872f5
kinkax <jawahar.reddy007@gmail.com>
2016-04-04 13:18:12 +0530

    logout fixes
    hopefully

---
+++ js/init.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+      $('.modal-trigger').leanModal({
+       dismissible: true,
+       opacity: .5,
+       in_duration: 200,
+       out_duration: 200,
+       starting_top: '10%'
+    });
+  });
--- js/login_logout.js
+++ js/login_logout.js
@@ -9,8 +9,8 @@ var getCookie = function(cname) {
     return "";
 }	
 var check_uname = function() {
-	$('#profile-name').text('blah blah');
 	$('#profile-name').addClass('hide');//hide the profile name by default
+	$('#login-btn').removeClass('hide');
 	var ck_value = getCookie('username');
 	if(ck_value.length>0) { //cookie exists, therefore login must be have been succesful
 		$('#login-btn').addClass('hide');
@@ -23,6 +23,13 @@ var check_uname = function() {
 		if(erck.length>0) {
 			document.cookie = "loginfailed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 			Materialize.toast(erck, 4000);
+			$('.modal-trigger').leanModal({
+		       dismissible: true,
+		       opacity: .5,
+		       in_duration: 200,
+		       out_duration: 200,
+		       starting_top: '10%'
+		    });
 			location.reload();
 		}
 	}	
