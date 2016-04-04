7346e750bea5ca88b9bd5f760a77e4c820b1a280
Shikhar Srivastava <shikharsrivastav@gmail.com>
2016-04-04 13:40:42 +0530

    FINALLY fixed it...... :)) Login Logout

--- js/login_logout.js
+++ js/login_logout.js
@@ -41,7 +41,7 @@ var check_uname = function() {
 }
 var logout = function() {
 	document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-	window.location.href("index.html");
+	window.location.href="index.html";
 }
 $(document).ready(function() {
 	  check_uname();
