e473c6172720bacf90d6200f066e6ae2753434db
Shikhar Srivastava <shikharsrivastav@gmail.com>
2016-04-08 11:24:38 +0530

    Small edits still

--- WEB-INF/classes/InsertServlet.java
+++ WEB-INF/classes/InsertServlet.java
@@ -12,7 +12,7 @@ import java.io.*;
 /*Some Documentation:
         
             ERRORMESSAGEs in Cookies Types:
-             1) insertfailed           
+             1) insertfailed: NEED TO BE HANDLED IN BOTH CTYPE.HTML and INDEX.HTML!!!**__**__**__**     
              
 */
 
@@ -24,8 +24,40 @@ public class InsertServlet extends HttpServlet
         response.setContentType("text/html");  
         System.out.println("In InsertServlet");
         String cType=request.getParameter("cType");  
-        
+        int preferences[]= new int[10];
+        String cName[]= new String[10];
+        for(int j=0;j<10;j++){prefernces[j]=-1;cName[j]=null;}
+        int i=-1;
+        do
+        {
+            i++;
+            cName[i]=request.getParameter("con-field-"+String.toString(i+1));
+            preferences[i]=Integer.parseInt(request.getParameter("rating-"+String.toString(i+1)));
+        }while(preferences[i]!=-1 && cName[i]!=null && i<10);
+        // i= 1 more than no. of ratings
+        int pref_count=i;
         String errorMsg = null;
+        Cookie[] cookies= request.getCookies();
+        String name=null;
+
+
+        for (Cookie ck: cookies) {
+            if("username".equals(ck.getName()))
+                name=ck.getValue();
+        }
+        if(name==null) {
+            errorMsg="No User Logged In!";  //changed to use this directly as the error message
+            Cookie cookey = new Cookie("insertfailed", errorMsg);
+            cookey.setMaxAge(60*2); 
+            response.addCookie(cookey);
+            response.sendRedirect(request.getContextPath()+"/index.html");
+        }
+        if(name=="kinkax") {
+    
+                System.out.println("Exiting 'Kinkax'");
+            response.sendRedirect(request.getContextPath()+"/results.html");
+           //return;  
+        }
 
           // JDBC driver name and database URL
            String JDBC_DRIVER="oracle.jdbc.driver.OracleDriver";  
@@ -36,6 +68,8 @@ public class InsertServlet extends HttpServlet
               //Change this BELOW to change the HTML file Format
             PreparedStatement ps = null;
             ResultSet rs = null;
+            PreparedStatement ps_check = null;
+            ResultSet rs_check = null; 
             PreparedStatement ps_main = null;
             ResultSet rs_main = null;
         try{
@@ -52,57 +86,84 @@ public class InsertServlet extends HttpServlet
                 {
                     System.out.println("Couldn't connect to Database");
                 }
-                ps = conn.prepareStatement("select * from USER_MAIN where USER_ID=?");
-                ps.setString(1, name);
-                rs = ps.executeQuery();
-               // System.out.println("After Query Execution"); 
-                if(rs != null && rs.next()) { 
-                    errorMsg="User with given UserName already exists.";  //changed to use this directly as the error message
-                    Cookie cookey = new Cookie("signup", errorMsg);
-                    cookey.setMaxAge(60); 
-                    response.addCookie(cookey);
-                    response.sendRedirect(request.getContextPath()+"/index.html");
-                    //For testing purposer->
-                    //System.out.println("User and password invalid!");
-                }
-                else
+                i=0
+                while(i<pref_count)
                 {
-                        ps_main = conn.prepareStatement("insert into USER_MAIN values(?,?,?,?)");
-                        ps_main.setString(1,name);
-                        ps_main.setString(3,password);
-                        ps_main.setString(4,age);
-                        ps_main.setString(2,String.valueOf(gender.charAt(0)));
-                        rs_main = ps_main.executeQuery();
-                        if(rs_main==null) { 
-                            errorMsg="Insertion into Table failed";  //changed to use this directly as the error message
-                            Cookie cookey = new Cookie("signup", errorMsg);
+                    ps = conn.prepareStatement("select count(*) as count_val from "+cType+" where title = ?";
+                    ps.setString(1,cName[i]);
+                    rs = ps.executeQuery();
+                       // System.out.println("After Query Execution"); 
+                    int count_val;  
+/*MIGHT CAUSE ERROR*/       if(rs== null ||((count_val=rs.getInt("count_val"))==0)) { 
+/*Change Error output?*/    errorMsg="Your choice of "+cType+"("+String.toString(i+1)+")"+" is unique Sir! It doesn't exist in our database.";  //changed to use this directly as the error message
+                            Cookie cookey = new Cookie("insertfailed", errorMsg);
                             cookey.setMaxAge(60); 
                             response.addCookie(cookey);
-                            response.sendRedirect(request.getContextPath()+"/index.html");
+                            response.sendRedirect(request.getContextPath()+"/"+cType+".html");
+                                                
                         }
-                       else if(errorMsg==null)
-                       {
-                        Cookie cookey = new Cookie("signup", successMsg);
-                        cookey.setMaxAge(60*2); 
-                        response.addCookie(cookey);
-                        response.sendRedirect(request.getContextPath()+"/index.html");
-                       }
 
-                }
-           }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
+                       else if(count_val>1)
+                         { 
+                            errorMsg="The "+cType+"("+String.toString(i+1)+")"+" name used is ambiguous!";  //changed to use this directly as the error message
+                            Cookie cookey = new Cookie("insertfailed", errorMsg);
+                            cookey.setMaxAge(60); 
+                            response.addCookie(cookey);
+                            response.sendRedirect(request.getContextPath()+"/"+cType+".html");
+                            
+                        }
+                        else 
+                        {
+                              ps_check= conn.prepareStatement("select count(*) as count_check from user_"+cType+" natural join "+cType+" where title =? and user_id=?";
+                              ps_check.setString(1,cName[i]);
+                              ps_check.setString(2,name);
+                              rs_check = ps_check.executeQuery();    
+                              int val_check= rs_check.getInt("count_check");
+                              if(val_check!=0)
+                              {
+                                   errorMsg="You have already rated "+cType+"("+String.toString(i+1)+")"+" before!";  //changed to use this directly as the error message
+                                    Cookie cookey = new Cookie("insertfailed", errorMsg);
+                                    cookey.setMaxAge(60); 
+                                    response.addCookie(cookey);
+                                    response.sendRedirect(request.getContextPath()+"/"+cType+".html");
+                              }
 
-            finally 
-            {
-                try
-                 {
-                    if(rs!=null) rs.close();
-                    if(ps!=null) ps.close();
-                    if(rs_main!=null) rs_main.close();
-                    if(ps_main!=null) ps_main.close();
-                  }catch(Exception e){e.printStackTrace();}
-            } 
+                                ps_main = conn.prepareStatement("insert into USER_"+cType+" values(?,?,?,?)");
+                                ps_main.setString(1,name);
+                                ps_main.setString(3,password);
+                                ps_main.setString(4,age);
+                                ps_main.setString(2,String.valueOf(gender.charAt(0)));
+                                rs_main = ps_main.executeQuery();
+                                if(rs_main==null) { 
+                                    errorMsg="Insertion into Table failed";  //changed to use this directly as the error message
+                                    Cookie cookey = new Cookie("insertfailed", errorMsg);
+                                    cookey.setMaxAge(60); 
+                                    response.addCookie(cookey);
+                                    response.sendRedirect(request.getContextPath()+"/index.html");
+                                }
+                               else if(errorMsg==null)
+                               {
+                                Cookie cookey = new Cookie("signup", succe);
+                                cookey.setMaxAge(60*2); 
+                                response.addCookie(cookey);
+                                response.sendRedirect(request.getContextPath()+"/index.html");
+                               }
+
+                        }
+                   }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    finally 
+                    {
+                        try
+                         {
+                            if(rs!=null) rs.close();
+                            if(ps!=null) ps.close();
+                            if(rs_main!=null) rs_main.close();
+                            if(ps_main!=null) ps_main.close();
+                          }catch(Exception e){e.printStackTrace();}
+                    } 
     }
 }
