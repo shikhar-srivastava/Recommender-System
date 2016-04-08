78113cd957fc3948931a4bd0e0913532384bac13
kinkax <jawahar.reddy007@gmail.com>
2016-04-08 18:27:22 +0530

    handling insert fail

--- js/init_content.js
+++ js/init_content.js
@@ -1,5 +1,17 @@
 var i = 3;
 
+var getCookie = function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+} 
+
+
 $('#add-field').click(function() {
   if(i<=10) {
 //    Materialize.toast("What is life without pizza?",4000);
@@ -13,6 +25,12 @@ $('#add-field').click(function() {
     $('#add-btn').addClass('disabled').removeClass("waves-effect waves-light");
   }
 });
+
+var insert_check=function() { //check the sign-up status
+  var msg = getCookie('insertfailed');
+  Materialize.toast(msg,3000);
+  document.cookie = "insertfailed=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+}
 
 $(document).ready(function() {
       $('.modal-trigger').leanModal({
