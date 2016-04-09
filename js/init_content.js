6c9fe46509394b06261565db2456f44b682d3b6e
kinkax <jawahar.reddy007@gmail.com>
2016-04-09 11:37:36 +0530

    fixed add-field function

--- js/init_content.js
+++ js/init_content.js
@@ -17,7 +17,7 @@ $('#add-field').click(function() {
 //    Materialize.toast("What is life without pizza?",4000);
     var form = $('#content-form');
     i++;
-    $('<div class="row"><div class="input-field col s9"><input type="text" class="grey lighten-5" name="con-field-'+i+'" id="con-field-'+i+'" size=255/></div><div class="input-field col s3"><p class="range-field"><input type="range" style="padding-top: 5px" id="rating-'+i+'" min="0" max="5" /></p></div></div>').prependTo(form);
+    $('<div class="row"><div class="input-field col s9"><input type="text" class="grey lighten-5" name="con-field-'+i+'" id="con-field-'+i+'" size=255/></div><div class="input-field col s3"><p class="range-field"><input type="range" style="padding-top: 5px" id="rating-'+i+'" name="rating-'+i+'" min="0" max="5" /></p></div></div>').prependTo(form);
     return false;
   }
   else {
